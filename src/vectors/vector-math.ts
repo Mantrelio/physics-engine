ec781d7181ac9b345b75ee54fec0e6aae86547c3
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-07-31 18:30:40 +0300

    implement 2D vectors with simple math operations

---
+++ src/vectors/vector-math.ts
@@ -0,0 +1,53 @@
+import { Vector } from "./vector";
+
+export class VectorMath {
+    static add(...vectors: Vector[]): Vector {
+        if (vectors.length < 2) throw new Error(`Add() method expected at least 2 vectors, got ${vectors.length}`);
+
+        let xValue: number = 0;
+        let yValue: number = 0; 
+
+        for (const vector of vectors) {
+            xValue += vector.x;
+            yValue += vector.y;
+        }
+
+        return new Vector(xValue, yValue);
+    }
+
+    static subtract(...vectors: Vector[]): Vector {
+        if (vectors.length < 2) throw new Error(`Subtract() method expected at least 2 vectors, got ${vectors.length}`);
+
+        let xValue: number = vectors[0].x;
+        let yValue: number = vectors[0].y; 
+
+        for (let i = 1; i < vectors.length; i++) {
+            xValue -= vectors[i].x;
+            yValue -= vectors[i].y;
+        }
+
+        return new Vector(xValue, yValue);
+    }
+
+    static multiply(vector: Vector, scalar: number): Vector {
+        return new Vector(vector.x * scalar, vector.y * scalar);
+    }
+
+    static dot(vector1: Vector, vector2: Vector): number {
+        return (vector1.x * vector2.x + vector1.y * vector2.y);
+    }
+
+    static cross(vector1: Vector, vector2: Vector): number {
+        return (vector1.x * vector2.y - vector1.y * vector2.x);
+    }
+
+    static rotate(vector: Vector, angleRadians: number): Vector {
+        const cos = Math.cos(angleRadians);
+        const sin = Math.sin(angleRadians);
+
+        return new Vector(
+            vector.x * cos - vector.y * sin,
+            vector.x * sin + vector.y * cos
+        );
+    }
+}
