fef94dc0a6d1beb644732d8b1b0206c5ea35ab47
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-08-01 22:16:51 +0300

    update force object, magnitude calculation is automatic

--- src/forces/force.ts
+++ src/forces/force.ts
@@ -1,11 +1,11 @@
 import { Vector } from "../vectors/vector";
+import { VectorMath } from "../vectors/vector-math";
 
 export class Force extends Vector {
-    constructor(
-        x: number,
-        y: number,
-        public readonly newtons: number
-    ) {
+    public readonly magnitude: number;
+
+    constructor(x: number, y: number) {
         super(x, y)
+        this.magnitude = VectorMath.magnitude(this);
     }
 }
