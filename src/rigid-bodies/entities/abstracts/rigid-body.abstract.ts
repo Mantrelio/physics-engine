72f14b39a2961cb690928ebe7d7833a1a536491e
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-08-03 19:23:41 +0300

    refactor rigid bodies

--- src/rigid-bodies/entities/abstracts/rigid-body.abstract.ts
+++ src/rigid-bodies/entities/abstracts/rigid-body.abstract.ts
@@ -1,21 +1,23 @@
+import { BaseRenderData } from "../../../renderer/interfaces/render-data.interface";
 import { Vector } from "../../../vectors/entities/vector";
 import { VectorMath } from "../../../vectors/vector-math";
 
 export abstract class RigidBody {
     constructor(
         public position: Vector,
-        public velocity: Vector = new Vector(0, 0),
-        public mass: number = 1,
-        public acceleration: Vector = new Vector(0, 0)
+        private velocity: Vector = new Vector(0, 0),
+        private mass: number = 1,
+        private acceleration: Vector = new Vector(0, 0)
     ) {}
 
     private forces: Vector[] = [];
 
+    //TODO: assign public for clarification
     addForce(force: Vector): void {
         this.forces.push(force);
     }
 
-    updatePosition(deltaTime: number): void {
+    public updatePosition(deltaTime: number): void {
         const lastAcceleration: Vector = this.acceleration;    
 
         this.position = VectorMath.add(this.position, VectorMath.multiply(this.velocity, deltaTime), VectorMath.multiply(this.acceleration, deltaTime * deltaTime * 0.5));
@@ -27,4 +29,6 @@ export abstract class RigidBody {
 
         this.forces = [];
     }
+
+    abstract getRenderData(): BaseRenderData;
 }
