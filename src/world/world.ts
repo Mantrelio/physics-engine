9a6f3b7402e580687518f8ba673e023cf4346432
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-08-11 20:51:08 +0300

    implement air resistance

--- src/world/world.ts
+++ src/world/world.ts
@@ -2,7 +2,9 @@ import { CollisionDetection } from "../collisions/collision-detection";
 import { CanvasRenderer } from "../renderer/canvas-renderer";
 import { RigidBody } from "../rigid-bodies/abstracts/rigid-body.abstract";
 import { Circle } from "../rigid-bodies/circle";
+import { Shape } from "../rigid-bodies/enums/shape.enum";
 import { Vector } from "../vectors/entities/vector";
+import { VectorMath } from "../vectors/vector-math";
 
 export class World {
     private objects: RigidBody[] = [];
@@ -15,7 +17,12 @@ export class World {
     private accumulator: number = 0;
     private readonly physicsStepsLimit: number = 4;
 
+    private readonly dragCoefficients: Record<Shape, number> = {
+        'circle': 0.47
+    }
+
     constructor(
+        private readonly airDensity: number,
         canvasId: string,
     ) {
         const canvas = document.getElementById(canvasId) as HTMLCanvasElement;
@@ -58,6 +65,7 @@ export class World {
         let physicsStepsCount: number = 0;
 
         while(this.accumulator >= this.fixedTimeStep && physicsStepsCount < this.physicsStepsLimit) {
+            this.applyDrag();
             this.updatePhysics(this.fixedTimeStep);
             this.collisionDetection.checkForCollision(this.objects);
 
@@ -82,31 +90,50 @@ export class World {
     }
 
     public applyGravity(): void {
-        const g = new Vector(0, 9.8)
+        const g = new Vector(0, 9.8);
         this.objects.forEach(object => object.addAcceleration(g));
     }
 
     private applyConstraints(rigidBody: RigidBody): void {
+        const restitution = 0.8;
+
         if(rigidBody instanceof Circle) {
             if(rigidBody.position.x - rigidBody.radius <= 0) {
                 rigidBody.position.x = rigidBody.radius;
-                rigidBody.velocity.x = -rigidBody.velocity.x;
+                rigidBody.velocity.x = -rigidBody.velocity.x * restitution;
             }
 
             if(rigidBody.position.x + rigidBody.radius >= this.canvasWidth) {
                 rigidBody.position.x = this.canvasWidth - rigidBody.radius;
-                rigidBody.velocity.x = -rigidBody.velocity.x;
+                rigidBody.velocity.x = -rigidBody.velocity.x * restitution;
             }
 
             if(rigidBody.position.y - rigidBody.radius <= 0) {
                 rigidBody.position.y = rigidBody.radius;
-                rigidBody.velocity.y = -rigidBody.velocity.y;
+                rigidBody.velocity.y = -rigidBody.velocity.y * restitution;
             }
 
             if(rigidBody.position.y + rigidBody.radius >= this.canvasHeight) {
                 rigidBody.position.y = this.canvasHeight - rigidBody.radius;
-                rigidBody.velocity.y = -rigidBody.velocity.y;
+                rigidBody.velocity.y = -rigidBody.velocity.y * restitution;
             }
         }
+    }
+
+    private applyDrag(): void {
+        this.objects.forEach(object => {
+            if (object.velocity.x === 0 && object.velocity.y === 0) {
+                return;
+            }
+                
+            if (object instanceof Circle) {
+                const dragAcceleration: Vector = VectorMath.multiply(
+                    VectorMath.multiply(object.velocity, 0.5*this.airDensity*object.radius*VectorMath.magnitude(object.velocity)*this.dragCoefficients[object.shape] / object.mass),
+                    -1
+                );
+
+                object.velocity = VectorMath.add(object.velocity, VectorMath.multiply(dragAcceleration, this.fixedTimeStep));
+            }
+        });
     }
 }
