54165edc9f5472a859cf4fc4f584e024a65a517f
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-08-13 13:19:52 +0300

    feat: add in-place addition method to Vector class

--- src/vectors/entities/vector.ts
+++ src/vectors/entities/vector.ts
@@ -3,4 +3,9 @@ export class Vector {
         public x: number,
         public y: number
     ) {}
+
+    public add(vector: Vector) {
+        this.x = this.x + vector.x;
+        this.y = this.y + vector.y;
+    }
 }
