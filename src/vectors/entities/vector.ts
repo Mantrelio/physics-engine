963775de3617d00caf13bc1b51427e94ca58448e
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-08-13 14:14:19 +0300

    feat: implement return self to Vector class methods

--- src/vectors/entities/vector.ts
+++ src/vectors/entities/vector.ts
@@ -4,39 +4,51 @@ export class Vector {
         public y: number
     ) {}
 
-    public add(vector: Vector): void {
+    public add(vector: Vector): Vector {
         this.x = this.x + vector.x;
         this.y = this.y + vector.y;
+
+        return this;
     }
 
-    public subtract(vector: Vector): void {
+    public subtract(vector: Vector): Vector {
         this.x = this.x - vector.x;
         this.y = this.y - vector.y;
+
+        return this;
     }
 
-    public multiply(scalar: number): void {
+    public multiply(scalar: number): Vector {
         this.x = this.x * scalar;
         this.y = this.y * scalar;
+
+        return this;
     }
 
-    public divide(scalar: number): void {
+    public divide(scalar: number): Vector {
         this.x = this.x / scalar;
         this.y = this.y / scalar;
+
+        return this;
     }
 
     public magnitude(): number {
         return Math.sqrt(this.x * this.x + this.y * this.y);
     }
 
-    public rotate(angleRadians: number): void {
+    public rotate(angleRadians: number): Vector {
         const cos = Math.cos(angleRadians);
         const sin = Math.sin(angleRadians);
 
         this.x = this.x * cos - this.y * sin;
         this.y = this.x * sin + this.y * cos;
+
+        return this;
     }
 
-    public normalize(): void {
+    public normalize(): Vector {
         this.divide(this.magnitude());
+
+        return this;
     }
 }
