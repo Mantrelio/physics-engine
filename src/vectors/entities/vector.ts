6487736f31029eee890e0c17c1421e09163b4bb8
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-08-13 13:38:12 +0300

    refactor: add return types to Vector class methods

--- src/vectors/entities/vector.ts
+++ src/vectors/entities/vector.ts
@@ -4,22 +4,22 @@ export class Vector {
         public y: number
     ) {}
 
-    public add(vector: Vector) {
+    public add(vector: Vector): void {
         this.x = this.x + vector.x;
         this.y = this.y + vector.y;
     }
 
-    public subtract(vector: Vector) {
+    public subtract(vector: Vector): void {
         this.x = this.x - vector.x;
         this.y = this.y - vector.y;
     }
 
-    public multiply(scalar: number) {
+    public multiply(scalar: number): void {
         this.x = this.x * scalar;
         this.y = this.y * scalar;
     }
 
-    public divide(scalar: number) {
+    public divide(scalar: number): void {
         this.x = this.x / scalar;
         this.y = this.y / scalar;
     }
