a059fe71d9c764676d15ac6f319099a9b9604868
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-08-18 16:45:00 +0300

    feat: implement axis aligned bounding boxes

---
+++ src/collisions/axis-aligned-bounding-box.ts
@@ -0,0 +1,27 @@
+import { Vector } from "../vectors/entities/vector";
+
+export class AABB {
+    constructor(
+        public readonly position: Vector,
+        public readonly halfWidth: number,
+        public readonly halfHeight: number
+    ) {}
+
+    public containsPoint(pointPosition: Vector): boolean {
+        return (
+            pointPosition.x >= this.position.x - this.halfWidth &&
+            pointPosition.x <= this.position.x + this.halfWidth &&
+            pointPosition.y >= this.position.y - this.halfHeight &&
+            pointPosition.y <= this.position.y + this.halfHeight
+        );
+    }
+
+    public intersects(range: AABB): boolean {
+        return !(
+            range.position.x - range.halfWidth > this.position.x + this.halfWidth ||
+            range.position.x + range.halfWidth < this.position.x - this.halfWidth ||
+            range.position.y + range.halfHeight < this.position.y - this.halfHeight ||
+            range.position.y - range.halfHeight > this.position.y + this.halfHeight 
+        );
+    }
+}
