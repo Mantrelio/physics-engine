5dc7f3868eb5a91e6685a7bacc2ac9dd35af432d
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-08-19 18:27:40 +0300

    fix: correct AABB drawing to utilize optional color property

--- src/renderer/drawers/gizmo-drawer.ts
+++ src/renderer/drawers/gizmo-drawer.ts
@@ -4,7 +4,7 @@ import { AABBRenderData } from "../interfaces/render-data.interface";
 export class GizmoDrawer {
     static drawAABB(ctx: CanvasRenderingContext2D, data: AABBRenderData): void {
         ctx.save();
-        ctx.strokeStyle = 'rgba(19, 148, 2, 0.5)';
+        ctx.strokeStyle = data.color || 'rgba(19, 148, 2, 0.5)';
         ctx.lineWidth = 1;
         ctx.strokeRect(
             data.x - data.halfWidth,
