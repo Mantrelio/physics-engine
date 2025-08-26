f2eae89a67d25cb6420c07a322edaece063b056a
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-08-26 18:20:51 +0300

    feat: implement collision resolver

--- src/collisions/collision-resolver.ts
+++ src/collisions/collision-resolver.ts
@@ -1,26 +1,55 @@
 import { RigidBody } from "../rigid-bodies/abstracts/rigid-body.abstract";
-import { Circle } from "../rigid-bodies/circle";
 import { Vector } from "../vectors/entities/vector";
 import { VectorMath } from "../vectors/vector-math";
+import { CollisionData } from "./types/collision-data.type";
 
 export class CollisionResolver {
-    private readonly CollisionResolutionHandles: Record<string, (rigidBodyA: RigidBody, rigidBodyB: RigidBody) => void> = {
-        "circle-circle": (rigidBodyA, rigidBodyB) => CollisionResolver.resolveCircleCircleCollision(rigidBodyA as Circle, rigidBodyB as Circle),
+    static execute(collisionData: CollisionData): void {
+        const { objectA, objectB, peneterationDepth: penetrationDepth, collisionNormal } = collisionData;
+
+        const relativeVelocity: Vector = VectorMath.subtract(objectB.velocity, objectA.velocity);
+        const velocityAlongNormalScalar: number = VectorMath.dot(relativeVelocity, collisionNormal);
+
+        if (velocityAlongNormalScalar > 0) return;
+
+        const restitution: number = 0.8;
+
+        const inverseObjectAMass: number = 1 / objectA.mass;
+        const inverseObjectBMass: number = 1 / objectB.mass; 
+
+        const impulseScalar: number = -(1 + restitution) * velocityAlongNormalScalar / (inverseObjectAMass + inverseObjectBMass);
+        const impulseVector: Vector = VectorMath.multiply(collisionNormal, impulseScalar);
+
+        this.resolveObjectPositions(objectA, objectB, penetrationDepth, inverseObjectAMass, inverseObjectBMass, collisionNormal);
+        this.resolveObjectVelocities(objectA, objectB, inverseObjectAMass, inverseObjectBMass, impulseVector);
+    }
+    
+    private static resolveObjectPositions(
+        objectA: RigidBody, 
+        objectB: RigidBody, 
+        penetrationDepth: number, 
+        inverseObjectAMass: number, 
+        inverseObjectBMass: number, 
+        collisionNormal: Vector
+    ): void {
+        const percent = 0.8; 
+        const slop = 0.01;
+
+        const correctionMagnitude = Math.max(penetrationDepth - slop, 0) / (inverseObjectAMass + inverseObjectBMass) * percent;
+        const correction = VectorMath.multiply(collisionNormal, correctionMagnitude);       
+
+        objectA.position.subtract(VectorMath.multiply(correction, inverseObjectAMass));
+        objectB.position.add(VectorMath.multiply(correction, inverseObjectBMass));
     }
 
-    static resolveCircleCircleCollision(circleA: Circle, circleB: Circle): void {
-        const radii: number = circleA.radius + circleB.radius;
-        const distanceVector: Vector = VectorMath.subtract(circleA.position, circleB.position);
-        const peneteration: number = radii - VectorMath.magnitude(distanceVector);
-        const correctionDistanceVector: Vector = VectorMath.multiply(VectorMath.normalize(distanceVector), peneteration / 2);
-
-        circleB.position.subtract(correctionDistanceVector);
-        circleA.position.add(correctionDistanceVector);
-
-        const v1 = VectorMath.dot(circleA.velocity, VectorMath.normalize(distanceVector));
-        const v2 = VectorMath.dot(circleB.velocity, VectorMath.normalize(distanceVector));
-
-        circleA.velocity.add(VectorMath.multiply(VectorMath.normalize(distanceVector), v2 - v1));
-        circleB.velocity.add(VectorMath.multiply(VectorMath.normalize(distanceVector), v1 - v2)); 
+    private static resolveObjectVelocities(
+        objectA: RigidBody, 
+        objectB: RigidBody,
+        inverseObjectAMass: number,
+        inverseObjectBMass: number,
+        impulseVector: Vector
+    ): void {
+        objectA.velocity.subtract(VectorMath.multiply(impulseVector, inverseObjectAMass));
+        objectB.velocity.add(VectorMath.multiply(impulseVector, inverseObjectBMass));
     }
 }
