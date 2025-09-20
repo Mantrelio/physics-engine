d218212c10a91595ad20eeb0f38fd243935945a1
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-09-20 21:46:28 +0300

    feat: implement package entry point

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { PhysicsEngine } from './physics-engine/physics-engine';
+export { RigidBodyFactory } from './rigid-bodies/factories/rigidbody-factory';
+export { Vector } from './vectors/entities/vector';
+export { VectorMath } from './vectors/vector-math';
